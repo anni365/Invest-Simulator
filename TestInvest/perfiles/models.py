59aabeaa20380701e20b733515364e7a3b95e602
Ashley Quezada <ashley_quezada@hotmail.com>
2018-10-15 14:07:34 -0300

    Se agrega la clase Wallet para saber el monto de dinero y sus activos, por lo tanto tambien se agrega la clase Asset para su relacion.

--- TestInvest/perfiles/models.py
+++ TestInvest/perfiles/models.py
@@ -13,10 +13,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
-class Perfil(models.Model):
-    usuario = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=255, blank=True)
-    web = models.URLField(blank=True)
+class Wallet(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    virtual_money = models.PositiveIntegerField(blank=None)
+    virtual_money = 1000
 
     # Python 3
     def __str__(self):
@@ -31,3 +31,11 @@ def crear_usuario_perfil(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def guardar_usuario_perfil(sender, instance, **kwargs):
     instance.perfil.save()
+
+class Asset(models.Model):
+    user = models.ForeignKey(CustomUser, default='', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    cant = models.PositiveIntegerField(blank=None)
+    old_unit_value = models.PositiveIntegerField(blank=None)
+    date = models.DateTimeField('date published')
+
