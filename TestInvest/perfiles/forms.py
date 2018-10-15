f1f618b5ccce88962a9bfe0ac2967ec5aee26fc6
Cami Cuñer <camicba1@gmail.com>
2018-10-15 12:58:14 -0300

    Subiendo nueva carpeta con proyecto.

---
+++ TestInvest/perfiles/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.forms import (
+  UserCreationForm, AuthenticationForm, PasswordChangeForm)
+from django.contrib.auth.models import User
+from .models import CustomUser
+from django.contrib.auth.forms import UserChangeForm
+from django.core.files.images import get_image_dimensions
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(label="Nombre", max_length=140, required=True)
+    last_name = forms.CharField(label="Apellido", max_length=140,
+                                required=True)
+    email = forms.EmailField(required=True)
+    email2 = forms.EmailField(label='Email (confirmacion)', required=True)
+
+    def clean_email2(self):
+        email = self.cleaned_data.get("email")
+        email2 = self.cleaned_data.get("email2")
+        if email and email != email2:
+            raise forms.ValidationError("Los 2 campos de emails no coinciden")
+        return email2
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'username',
+            'email',
+            'email2',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2',
+            'avatar',
+        )
+
