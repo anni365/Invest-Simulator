5701422eff295f26b67fd5f06203472889015e03
veronica412 <vad41284@hotmail.com>
2018-10-25 12:25:41 -0300

    Se modularizo buy_assets

--- TestInvest/perfiles/views.py
+++ TestInvest/perfiles/views.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render, redirect, render_to_response
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.contrib.auth import login, authenticate, update_session_auth_hash
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth.forms import PasswordChangeForm
 from django.utils import timezone
 from datetime import datetime
-
 from .forms import SignUpForm, BuyForm
 from django.contrib import messages
 from .models import CustomUser, UserAsset, Transaction
 import json
-from importlib import reload
 
 
 class SignUpView(CreateView):
@@ -72,13 +70,83 @@ def open_jsons():
 
 def calculate_capital(assets, my_assets, virtual_money):
     cap = 0
-    for name, dates in assets:
-        date = list(dates.values())
+    for name, prices in assets:
+        data = list(prices.values())
         for asset in my_assets:
-            if (asset.name == name[1] and date[1] is not None):
-                cap += asset.total_amount * date[1]
+            if (asset.name == name[1] and data[1] is not None):
+                cap += asset.total_amount * data[1]
     cap += virtual_money
     return cap
+
+
+def buy_assets(request, form, assets, capital):
+    virtual_money = request.user.virtual_money
+    if form.is_valid():
+        name = form.cleaned_data.get("name")
+        total_amount = form.cleaned_data.get("total_amount")
+        assets_user = UserAsset.objects.filter(user=request.user.id, name=name)
+        exist_asset = assets_user.exists()
+        for nametype, prices in assets:
+            data = list(prices.values())
+            if nametype[1] == name and (data[0] is None or data[1] is None):
+                messages.add_message(
+                  request, messages.INFO, 'El Activo seleccionado ya no se'
+                  'encuentra  disponible, no se pudo concretar la compra. Para'
+                  'ver la actual lista de activos recargue la pagina')
+                break
+            addOperation(request, exist_asset, assets_user, nametype, name,
+                         total_amount, data, virtual_money)
+            virtual_money = request.user.virtual_money
+        return virtual_money, assets
+
+
+def addOperation(request, exist_asset, assets_user, nametype, name_form,
+                 total_amount, data, virtual_money):
+    if exist_asset:
+        for asset in assets_user:
+            if (nametype[1] == asset.name):
+                update_asset(asset, total_amount, data)
+                transaction = addTransaction(
+                  request, data[0], data[1], total_amount, asset.id)
+                update_money_user(request, total_amount, data, virtual_money)
+    elif (nametype[1] == name_form):
+        asset_user = addAsset(request, name_form, total_amount, nametype[0],
+                              data[0])
+        transaction = addTransaction(
+          request, data[0], data[1], total_amount, asset_user.id)
+        update_money_user(request, total_amount, data, virtual_money)
+    return virtual_money
+
+
+def update_asset(asset, total_amount, data):
+    asset.total_amount += total_amount
+    asset.old_unit_value = data[0]
+    asset.save()
+
+
+def update_money_user(request, total_amount, data, virtual_money):
+    virtual_money -= total_amount * data[1]
+    request.user.virtual_money = virtual_money
+    request.user.save()
+
+
+def addTransaction(request, value_buy, value_sell, total_amount,
+                   user_asset_id):
+    transaction = Transaction.objects.create(
+                  user_id=request.user.id, user_asset_id=user_asset_id,
+                  value_buy=value_buy, value_sell=value_sell,
+                  amount=total_amount, date=datetime.now(tz=timezone.utc),
+                  type_transaction="compra")
+    transaction.save()
+    return transaction
+
+
+def addAsset(request, name, total_amount, type_asset, old_unit_value):
+    asset = UserAsset.objects.create(
+              user_id=request.user.id, name=name, total_amount=total_amount,
+              type_asset=type_asset, old_unit_value=old_unit_value)
+    asset.save()
+    return asset
 
 
 def show_assets(request):
@@ -103,59 +171,3 @@ def show_assets(request):
           'assets': assets, 'user': user, 'my_assets': my_assets,
           'capital': cap})
 
-
-def buy_assets(request, form, assets, capital):
-    user = CustomUser
-    virtual_money = request.user.virtual_money
-    if form.is_valid():
-        name = form.cleaned_data.get("name")
-        total_amount = form.cleaned_data.get("total_amount")
-        my_assets = UserAsset.objects.filter(user=request.user.id, name=name)
-        exist = my_assets.exists()
-        for names, dates in assets:
-            date = list(dates.values())
-            if names[1] == name and (date[0] is None or date[1] is None):
-                messages.add_message(
-                  request, messages.INFO, 'El Activo seleccionado ya no se'
-                  'encuentra  disponible, no se pudo concretar la compra. Para'
-                  'ver la acual lista de activos recargue la pagina')
-                break
-            if exist:
-                for asset in my_assets:
-                    if (names[1] == asset.name):
-                        asset.total_amount = asset.total_amount + total_amount
-                        asset.old_unit_value = date[0]
-                        asset.save()
-                        transaction = addTransaction(
-                          request, date[0], date[1], total_amount, asset.id)
-                        virtual_money = virtual_money - total_amount * date[1]
-                        request.user.virtual_money = virtual_money
-                        request.user.save()
-            elif (names[1] == name):
-                my_asset = addAsset(
-                  request, name, total_amount, names[0], date[0])
-                transaction = addTransaction(
-                  request, date[0], date[1], total_amount, my_asset.id)
-                virtual_money = virtual_money - total_amount * date[0]
-                request.user.virtual_money = virtual_money
-                request.user.save()
-        return virtual_money, assets
-
-
-def addTransaction(request, value_buy, value_sell, total_amount,
-                   user_asset_id):
-    transaction = Transaction.objects.create(
-                  user_id=request.user.id, user_asset_id=user_asset_id,
-                  value_buy=value_buy, value_sell=value_sell,
-                  amount=total_amount, date=datetime.now(tz=timezone.utc),
-                  type_transaction="compra")
-    transaction.save()
-    return transaction
-
-
-def addAsset(request, name, total_amount, type_asset, old_unit_value):
-    asset = UserAsset.objects.create(
-              user_id=request.user.id, name=name, total_amount=total_amount,
-              type_asset=type_asset, old_unit_value=old_unit_value)
-    asset.save()
-    return asset
