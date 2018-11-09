7cf5fb12eab4895ffe725483910fb5e3d3452524
Romina28 <romisalgado28@gmail.com>
2018-11-09 20:20:32 -0300

    agrego front para dar de baja las alarmas y las funciones js necesarias

--- TestInvest/perfiles/static/js/templates.js
+++ TestInvest/perfiles/static/js/templates.js
@@ -54,4 +54,40 @@
     event.preventDefault();
     mensajes();
   };
+  function view_alarm() {
+    document.getElementById("myPopup_alarm").style.display = "none";
+    document.getElementById("back").style['pointer-events'] = "auto";
+    document.getElementById("back").style.opacity = 1;
+    document.getElementById("menu_invest").style['pointer-events'] = "auto";
+    document.getElementById("menu_invest").style.opacity = 1;
+  };
+  function low_form(asset, umbral ,price) {
+    document.getElementById("myPopup_alarm").style.display = "block";
+    document.getElementById("back").style['pointer-events'] = "none";
+    document.getElementById("back").style.opacity = 0.4;
+    document.getElementById("menu_invest").style['pointer-events'] = "none";
+    document.getElementById("menu_invest").style.opacity = 0.4;
+    document.getElementById("id_name_low").disabled = true;
+    document.getElementById("id_name_low").value = asset;
+    document.getElementById("id_umbral_low").disabled = true;
+    document.getElementById("id_umbral_low").value = umbral;
+    document.getElementById("id_price_low").disabled = true;
+    document.getElementById("id_price_low").value = parseFloat(price);
+    setTimeout("enableDiv()",1);
+  };
+  function enableDiv() {
+    $("#body").click(function() {
+    window.location="http://localhost:8000/view_alarm";
+    });
+  }
+  function low_confirm(event) {
+    event.preventDefault();
+  };
+  function send_low() {
+    document.getElementById("id_name_low").disabled = false;
+    document.getElementById("id_umbral_low").disabled = false;
+    document.getElementById("id_price_low").disabled = false;
+    alert("Su alarma a sido dada de baja");
+    document.getElementById("my_form").submit();
+  };
 
